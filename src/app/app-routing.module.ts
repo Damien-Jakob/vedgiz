b5f1c2ea6ddd4367218465acc995810b5fa90727
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-04 08:34:13 +0200

    Add vegetables page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'vegetables',
+    loadChildren: () => import('./vegetables/vegetables.module').then( m => m.VegetablesPageModule)
+  },
 ];
 
 @NgModule({
