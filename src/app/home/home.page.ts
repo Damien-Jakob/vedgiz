557db4b35b8b3b524fc2e4f1e1c09c03bd85a463
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-05 08:54:52 +0200

    Add form base

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,27 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    private todo: FormGroup;
 
-  public welcomeMessage : string = "Bonjour";
-  public welcomePerson : string;
-  public isFemale : boolean;
-  public isVip : boolean;
-  public games : string[];
+    public welcomeMessage: string = "Bonjour";
+    public welcomePerson: string;
+    public isFemale: boolean;
+    public isVip: boolean;
+    public games: string[];
 
-  constructor() {
-    this.welcomePerson = "Lennon";
-    this.isFemale = false;
-    this.isVip = true;
-    this.games = [
-        "Skyrim",
-        "Totally accurate battles simulator",
-        "Minecraft!!",
-    ]
-  }
+    constructor(private formBuilder: FormBuilder) {
+        this.todo = this.formBuilder.group({
+            title: ['', Validators.required],
+            description: [''],
+        });
+
+        this.welcomePerson = "Lennon";
+        this.isFemale = false;
+        this.isVip = true;
+        this.games = [
+            "Skyrim",
+            "Totally accurate battles simulator",
+            "Minecraft!!",
+        ]
+    }
+
+    logForm() {
+        console.log(this.todo.value)
+    }
 
 }
