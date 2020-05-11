6f6a278d9e6a1807a15055ce7fa72c52007a43dc
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-11 18:45:28 +0200

    Add token detection in authentication service

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,21 +1,34 @@
 import {Injectable} from '@angular/core';
 import {HttpHeaders} from "@angular/common/http";
+import {isNotNullOrUndefined} from "codelyzer/util/isNotNullOrUndefined";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthenticationService {
-
     constructor() {
     }
 
-    protected token(): string {
+    public token(): string {
         // TODO get token from local storage
         const token: string = "25inl4IfpxLqjXqQ39rx78f8Fk3cJVwRE2EWz6ekBBE9hdU5k5U4CVPyM6W6";
         //const token : string = null;
         return token;
     }
 
+    public hasToken(): boolean {
+        return isNotNullOrUndefined(this.token());
+    }
+
+    /*
+    header :
+    authorization -> Bearer ~token~
+
+    Test of the token : try the /api/me API
+
+    * No token : Unauthenticated (401)
+    * Bad token : Invalid Token (401)
+    */
     public headers(): HttpHeaders {
         return this.headersFromToken(this.token());
     }
