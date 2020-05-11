7a943411068b9e66d87d92f7e65c1594ed0c5774
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-11 21:58:33 +0200

    Add api calls to api service

--- src/app/api-caller.service.ts
+++ src/app/api-caller.service.ts
@@ -1,11 +1,38 @@
 import {Injectable} from '@angular/core';
 import {AuthenticationService} from "./authentication.service";
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiCallerService {
+    protected API_ROOT: string = "http://127.0.0.1:8000/api/";
+    protected APPLY_API: string = "user/apply"
+    protected ME_API: string = "me";
 
-    constructor(protected authentication: AuthenticationService) {
+    constructor(
+        protected authentication: AuthenticationService,
+        protected http: HttpClient,
+    ) {
+
+    }
+
+    protected url(api: string): string {
+        return this.API_ROOT + api;
+    }
+
+    public apply(applicationData: object): Observable<object> {
+        return this.http.post(this.url(this.APPLY_API), applicationData);
+    }
+
+    public validateToken(token: string): Observable<object> {
+        const headers: HttpHeaders = this.authentication.headersFromToken(token);
+        return this.http.get(this.url(this.ME_API), {headers});
+    }
+
+    public me(): Observable<object> {
+        const headers: HttpHeaders = this.authentication.headers();
+        return this.http.get(this.url(this.ME_API), {headers});
     }
 }
