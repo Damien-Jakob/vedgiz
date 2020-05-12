a158d520a7b5c4d51a2e546c19983de33aa00d63
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-12 02:13:21 +0200

    Add user loading and display in profile

--- src/app/users/me/me.page.ts
+++ src/app/users/me/me.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiCallerService} from "../../api-caller.service";
+import {User} from "../../models/user";
+
+// TODO disply more informations -> look what the apis can do
 
 @Component({
-  selector: 'app-me',
-  templateUrl: './me.page.html',
-  styleUrls: ['./me.page.scss'],
+    selector: 'app-me',
+    templateUrl: './me.page.html',
+    styleUrls: ['./me.page.scss'],
 })
 export class MePage implements OnInit {
+    protected user: User;
 
-  constructor() { }
+    constructor(protected api: ApiCallerService) {
+        this.user = new User();
+        this.api.me().subscribe(
+            answer => {
+                this.user = answer.data;
+            }
+        )
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
