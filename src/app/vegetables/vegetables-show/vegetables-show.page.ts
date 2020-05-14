44e4a2415a0a7335504ffbf2de7bd52dcbf8824d
Damien Jakob <damien.jakob@cpnv.ch>
2020-05-14 17:49:30 +0200

    Get vegetable info

--- src/app/vegetables/vegetables-show/vegetables-show.page.ts
+++ src/app/vegetables/vegetables-show/vegetables-show.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Vegetable} from "../../models/vegetable";
+import {ApiCallerService} from "../../api-caller.service";
+import {AlertController} from "@ionic/angular";
 
 @Component({
-  selector: 'app-vegetables-show',
-  templateUrl: './vegetables-show.page.html',
-  styleUrls: ['./vegetables-show.page.scss'],
+    selector: 'app-vegetables-show',
+    templateUrl: './vegetables-show.page.html',
+    styleUrls: ['./vegetables-show.page.scss'],
 })
 export class VegetablesShowPage implements OnInit {
+    protected vegetable: Vegetable;
 
-  constructor() { }
+    constructor(
+        protected route: ActivatedRoute,
+        protected api: ApiCallerService,
+        protected alertController: AlertController
+    ) {
+        this.vegetable = new Vegetable();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        const vegetableId: string = this.route.snapshot.paramMap.get('id');
+        this.api.getProduct(vegetableId).subscribe(
+            answer => {
+                this.vegetable = answer.data;
+            },
+            error => {
+                this.alert("Erreur", "Le légume n'a pas pu être chargé.");
+            }
+        );
+    }
+
+    protected async alert(title: string, message: string) {
+        const alert = await this.alertController.create({
+            header: title,
+            //subHeader: 'Subtitle',
+            message: message,
+            buttons: ['OK'],
+        });
+
+        await alert.present();
+    }
 
 }
