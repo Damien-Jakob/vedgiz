f965fbbb3fd3961c2c30ed7fc4fef1f7cbda9d7a
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-23 11:39:32 +0200

    Add save/load methods to cart

--- src/app/cart-provider.service.ts
+++ src/app/cart-provider.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {DataProvider} from './data-provider.service';
 import {Vegetable} from './models/vegetable';
+import {Storage} from '@ionic/storage';
 
 @Injectable({
     providedIn: 'root'
@@ -8,7 +9,10 @@ import {Vegetable} from './models/vegetable';
 export class CartProvider {
     public content: Array<any>;
 
-    constructor(protected data: DataProvider) {
+    constructor(
+        protected data: DataProvider,
+        protected storage: Storage
+    ) {
         // TODO load cart from local storage, if there is one
         this.content = new Array<any>();
     }
@@ -25,7 +29,7 @@ export class CartProvider {
 
     public updateQuantity(vegetableId, quantity) {
         this.content.find(contentItem =>
-          contentItem.vegetableId === vegetableId
+            contentItem.vegetableId === vegetableId
         ).quantity = quantity;
     }
 
@@ -34,6 +38,27 @@ export class CartProvider {
         return this.data.vegetables.filter(vegetable =>
             // Is the vegetable not in the cart ?
             this.content.find(cartItem => cartItem.vegetable.id === vegetable.id) === undefined
+        );
+    }
+
+    // TODO
+    public save(): Promise<any> {
+        console.log('saving cart');
+        return this.storage.set('cart', this.content);
+    }
+
+    // TODO
+    public load(): Promise<any> {
+        const self: CartProvider = this;
+        return this.storage.get('cart').then(
+            (cart) => {
+                if (!(cart === null)) {
+                    self.content = cart;
+                } else {
+                    self.content = new Array<any>();
+                }
+                return self.content;
+            }
         );
     }
 }
