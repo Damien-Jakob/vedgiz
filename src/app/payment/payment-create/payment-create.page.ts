5658a10ba48bef4f0d5c1c7be92a8d25c20c4b44
Damien Jakob <damien.jakob@cpnv.ch>
2020-10-02 15:03:01 +0200

    Add payment form bases

--- src/app/payment/payment-create/payment-create.page.ts
+++ src/app/payment/payment-create/payment-create.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-payment-create',
-  templateUrl: './payment-create.page.html',
-  styleUrls: ['./payment-create.page.scss'],
+    selector: 'app-payment-create',
+    templateUrl: './payment-create.page.html',
+    styleUrls: ['./payment-create.page.scss'],
 })
 export class PaymentCreatePage implements OnInit {
+    // TODO init
+    private static envelopeKeyValidator;
+    private static amountValidator;
 
-  constructor() { }
+    private paymentForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+    ) {
+        this.paymentForm = this.formBuilder.group({
+            envelopeKey: ['', PaymentCreatePage.envelopeKeyValidator],
+            amount: ['50.00', PaymentCreatePage.amountValidator],
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    // TODO logic
+    private submitPaymentForm() {
+
+    }
 
 }
