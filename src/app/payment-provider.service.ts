4ced1bd38c7f4c5140673d0e7acd848ceca9e518
Damien Jakob <damien.jakob@cpnv.ch>
2020-10-06 13:34:25 +0200

    Add logic to payment-provider

--- src/app/payment-provider.service.ts
+++ src/app/payment-provider.service.ts
@@ -1,11 +1,24 @@
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class PaymentProviderService {
-    protected VEGETABLES_API = 'payments/';
+    protected PAYMENT_API = 'payments/';
 
-    constructor() {
+    constructor(
+        protected http: HttpClient,
+    ) {
+    }
+
+    public submitPayment(paymentData: object): Observable<any> {
+        return this.http.post(this.url(this.PAYMENT_API), paymentData);
+    }
+
+    protected url(api: string): string {
+        return environment.api_root + api;
     }
 }
