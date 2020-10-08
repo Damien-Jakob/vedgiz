f15631f00a66fd3abeb42356f75687a0637c16e0
Damien Jakob <damien.jakob@cpnv.ch>
2020-10-08 15:52:35 +0200

    Add payment submission

--- src/app/payment/payment-create/payment-create.page.ts
+++ src/app/payment/payment-create/payment-create.page.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PaymentProviderService} from '../../payment-provider.service';
+import {AlertController} from '@ionic/angular';
 
 @Component({
     selector: 'app-payment-create',
@@ -7,9 +9,9 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
     styleUrls: ['./payment-create.page.scss'],
 })
 export class PaymentCreatePage implements OnInit {
-    // TODO validate envelope
+    // TODO validate envelope (use invalid amount)
 
-    // TODO better validation envelope key
+    // TODO better validation of envelope key
     private static envelopeKeyValidator = [
         Validators.required,
         Validators.minLength(2)
@@ -17,17 +19,20 @@ export class PaymentCreatePage implements OnInit {
     // TODO better validation of amount
     private static amountValidator = [
         Validators.required,
-        Validators.min(0.00)
+        Validators.min(1),
+        Validators.pattern('^[1-9][0-9]*$'),
     ];
 
     private paymentForm: FormGroup;
 
     constructor(
+        private  payment: PaymentProviderService,
         private formBuilder: FormBuilder,
+        private alertController: AlertController,
     ) {
         this.paymentForm = this.formBuilder.group({
-            envelopeKey: ['', PaymentCreatePage.envelopeKeyValidator],
-            amount: ['50.00', PaymentCreatePage.amountValidator],
+            key: ['', PaymentCreatePage.envelopeKeyValidator],
+            amount: ['50', PaymentCreatePage.amountValidator],
         });
     }
 
@@ -36,7 +41,30 @@ export class PaymentCreatePage implements OnInit {
 
     // TODO logic
     private submitPaymentForm() {
-
+        this.payment.submitPayment(this.paymentForm.value).subscribe(
+            success => {
+                console.log('Payment accepted');
+            },
+            error => {
+                console.log(error);
+                if (error.status === 404) {
+                    this.alert('Erreur', `Numéro d'enveloppe inconnu`);
+                } else if (error.status === 400) {
+                    this.alert('Erreur', `Montant invalide`);
+                } else if (error.status === 403) {
+                    this.alert('Erreur', `Envelope déjà utilisée`);
+                }
+            });
     }
 
+    private async alert(title: string, message: string) {
+        const alert = await this.alertController.create({
+            header: title,
+            // subHeader: 'Subtitle',
+            message,
+            buttons: ['OK'],
+        });
+
+        await alert.present();
+    }
 }
