8cc8a8406f1e78fcce3d238266b0e5b278737945
Damien Jakob <damien.jakob@cpnv.ch>
2020-11-09 08:42:27 +0100

    Load vege list

--- src/app/stock/index/index.page.ts
+++ src/app/stock/index/index.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataProvider} from '../../data-provider.service';
+import {Vegetable} from "../../models/vegetable";
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.page.html',
-  styleUrls: ['./index.page.scss'],
+    selector: 'app-index',
+    templateUrl: './index.page.html',
+    styleUrls: ['./index.page.scss'],
 })
 export class IndexPage implements OnInit {
+    protected vegetablesToUpdate = new Array<Vegetable>();
+    protected displayedVegetableIndex: number;
 
-  constructor() { }
+    constructor(
+        protected data: DataProvider,
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    ionViewWillEnter() {
+        this.data.loadVegetables().then(
+            answer => {
+                console.log('Vegetables loaded');
+                console.log(answer);
+                this.vegetablesToUpdate = new Array<Vegetable>();
+                for (let i = 0; i < answer.length; i++) {
+                    this.vegetablesToUpdate[i] = answer[i];
+                }
+                this.displayedVegetableIndex = 0;
+                console.log(this.vegetablesToUpdate);
+            },
+        );
+    }
 }
